1abdea4add6f4275f5f77cd24d3c277d94c3c430
NidwijaB <nidwija.bhatta@cotiviti.com>
2023-09-26 15:26:19 +0000

    removed some files

--- file and exception handling/read_write_student_program.py
+++
@@ -1,9 +0,0 @@
-with open('student.txt','r') as source_file, open('new_student.txt','a') as dest_file:
-
-    # content=source_file.read()
-    # lines=source_file.readlines()
-    for line in source_file:
-        content_upper=line.upper()
-        dest_file.write(content_upper)
-print("data copied")
-print(source_file.closed)
